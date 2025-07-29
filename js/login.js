7855e38c0e3ff29d4eec157731de22fb3171cd87
sulistianaa <suliez.tiana16@gmail.com>
2025-07-29 13:36:46 +0700

    bismillah13

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Daftar akun simulasi
         const users = [
-            { email: "admin=", password: "admin123", role: "admin", redirect: "admin/admin.html" },
+            { email: "admin", password: "admin123", role: "admin", redirect: "admin/admin.html" },
             { email: "hc", password: "hc123", role: "hc", redirect: "hc/hc.html" },
             { email: "atasan", password: "atasan123", role: "atasan", redirect: "atasan/atasan.html" }
         ];
