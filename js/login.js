52ab5074c226b8990f4c4b01f6e68571d2aaab75
sulistianaa <suliez.tiana16@gmail.com>
2025-07-29 11:47:53 +0700

    bismillah 11

--- js/login.js
+++ js/login.js
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Daftar akun simulasi
         const users = [
-            { email: "admin", password: "admin123", role: "admin", redirect: "admin/admin.html" },
-            { email: "hc", password: "hc123", role: "hc", redirect: "hc/hc.html" },
-            { email: "atasan", password: "atasan123", role: "atasan", redirect: "atasan/atasan.html" }
+            { email: "admin@pertamina.com", password: "admin123", role: "admin", redirect: "admin/admin.html" },
+            { email: "hc@pertamina.com", password: "hc123", role: "hc", redirect: "hc/hc.html" },
+            { email: "atasan@pertamina.com", password: "atasan123", role: "atasan", redirect: "atasan/atasan.html" }
         ];
 
         // Cek apakah email & password cocok persis
