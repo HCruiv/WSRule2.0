1dce760075e465e5a5b4d6791562fe3287d268bc
sulistianaa <suliez.tiana16@gmail.com>
2025-07-29 09:30:36 +0700

    bismillah

---
+++ js/login.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); // Cegah reload
+
+        const email = document.getElementById("email").value.trim().toLowerCase();
+        const password = document.getElementById("password").value.trim();
+
+        if (email === "" || password === "") {
+            alert("Email dan password harus diisi!");
+            return;
+        }
+
+        // Daftar akun simulasi
+        const users = [
+            { email: "admin", password: "admin123", role: "admin", redirect: "../admin/admin.html" },
+            { email: "hc", password: "hc123", role: "hc", redirect: "../hc/hc.html" },
+            { email: "atasan", password: "atasan123", role: "atasan", redirect: "../atasan/atasan.html" }
+        ];
+
+        // Cek apakah email & password cocok persis
+        const user = users.find(u => u.email === email && u.password === password);
+
+        if (user) {
+            
+            window.location.href = user.redirect;
+        } else {
+            alert("Email atau password salah!");
+        }
+    });
+});
